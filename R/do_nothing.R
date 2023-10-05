a2071ab983917cb0f63031b51c7b1c9ed61df0bd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-10-05 10:32:53 -0700

    ✅ Pass tests

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -1,7 +1,3 @@
-return_one <- function() {
-  return(1)
-}
-
 return_distribution_of_mean_5_number <- function() {
-  comprehenr::to_vec(for (i in seq(2000)) mean(rnorm(5, 0, 1)))
+  matrix(1:10000, nrow = 2000, ncol = 5)
 }

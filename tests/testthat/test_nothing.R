94a82929a519bb7ed131dc49e6bbfdb3c713bf54
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-10-05 10:29:42 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -2,7 +2,10 @@ describe("Mean by row", {
   it("2000 means of 5 numbers", {
     expected_length <- 2000
     obtained <- return_distribution_of_mean_5_number()
-    obtained_length <- length(obtained)
+    obtained_length <- nrow(obtained)
+    obtained_cols <- ncol(obtained)
     expect_equal(obtained_length, expected_length)
+    expected_cols <- 5
+    expect_equal(obtained_cols, expected_cols)
   })
 })

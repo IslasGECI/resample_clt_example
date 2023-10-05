9454c28f446d5a9b0c9af20f86d85922a579dbe6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-10-05 09:36:28 -0700

    ♻️  Refactor

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,16 +1,8 @@
-describe("Test all is ready", {
-  it("Return one", {
-    expected <- 1
-    obtained <- return_one()
-    expect_equal(expected, obtained)
-  })
-})
-
-describe("Get version of the module", {
-  it("The version is 0.1.0", {
-    expected_version <- c("0.1.0")
-    obtained_version <- packageVersion("templater")
-    version_are_equal <- expected_version == obtained_version
-    expect_true(version_are_equal)
+describe("Mean by row", {
+  it("2000 means of 5 numbers", {
+    expected_length <- 2000
+    obtained <- return_distribution_of_mean_5_number()
+    obtained_length <- length(obtained)
+    expect_equal(obtained_length, expected_length)
   })
 })

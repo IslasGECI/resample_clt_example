d36772520bec6b85dcf8ea1daed96d6d8083d6fd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-10-05 10:51:27 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,15 +1,20 @@
 describe("Mean by row", {
-  it("2000 means of 5 numbers", {
+  obtained <- return_distribution_of_mean_5_number()
+  it("has the right dimension", {
     expected_length <- 2000
-    obtained <- return_distribution_of_mean_5_number()
     print(obtained)
     obtained_length <- nrow(obtained)
     obtained_cols <- ncol(obtained)
     expect_equal(obtained_length, expected_length)
     expected_cols <- 5
     expect_equal(obtained_cols, expected_cols)
-    expected_mean_first_column <- 0
+  })
+  it("The means of columns are differents", {
+    expected_mean_first_column <- 10
     obtained_mean_first_column <- mean(obtained[, 1])
-    expect_equal(obtained_mean_first_column, expected_mean_first_column, tolerance = 1e-3)
+    expect_equal(obtained_mean_first_column, expected_mean_first_column, tolerance = 1e-2)
+    expected_mean_first_column <- 20
+    obtained_mean_first_column <- mean(obtained[, 2])
+    expect_equal(obtained_mean_first_column, expected_mean_first_column, tolerance = 1e-2)
   })
 })
